704f14f92fd100ca961df79c40e18026d31b3ace
wassim ABOU DAHER <wassim.aboudaher.etu@univ-lille.fr>
2026-02-10 16:55:06 +0100

    Commit 2 : Mise à jour du Score et Exercice 0

--- types/score.ts
+++ types/score.ts
@@ -26,14 +26,52 @@ export const points = (
 
 // Exerice 0: Write all type constructors of Points, Deuce, Forty and Advantage types.
 
+export type FortyData = {
+  player: Player; // The player who have forty points
+  otherPoint: Point; // Points of the other player
+};
+
+export type Forty = {
+  kind: 'FORTY';
+  fortyData: FortyData;
+};
+
+export type Deuce = {
+  kind: 'DEUCE';
+};
+
+export type Advantage = {
+  kind: 'ADVANTAGE';
+  player: Player;
+};
+
 export type Game = {
   kind: 'GAME';
-  player: Player; // Player has won
+  player: Player;
 };
+
+// Exercice 0: Write all type constructors of Points, Deuce, Forty and Advantage types.
+
+export const deuce = (): Deuce => ({
+  kind: 'DEUCE',
+});
+
+export const forty = (player: Player, otherPoint: Point): Forty => ({
+  kind: 'FORTY',
+  fortyData: {
+    player,
+    otherPoint,
+  },
+});
+
+export const advantage = (player: Player): Advantage => ({
+  kind: 'ADVANTAGE',
+  player,
+});
 
 export const game = (winner: Player): Game => ({
   kind: 'GAME',
   player: winner,
 });
 
-export type Score = Points | Game;
+export type Score = Points | Forty | Deuce | Advantage | Game;
